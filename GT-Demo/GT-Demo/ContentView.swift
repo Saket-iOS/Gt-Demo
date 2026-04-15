59fa61772e6b6b38acb9faf5984d243f5ba7492d
Saket-iOS <saket@babytalkgpt.com>
2026-04-15 11:33:28 +0530

    Initial SetUp
    Push Blank Project

---
+++ GT-Demo/GT-Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GT-Demo
+//
+//  Created by Saket Pandhare on 15/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GT-Demo/GT-Demo/GT_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GT_DemoApp.swift
+//  GT-Demo
+//
+//  Created by Saket Pandhare on 15/04/26.
+//
+
+import SwiftUI
+
+@main
+struct GT_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
